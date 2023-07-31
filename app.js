436ea5d69a93eacf2f9f20ae7941762e158d6ddf
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-07-31 11:53:39 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function getPics() {}
+const test = document.getElementsByClassName("test-imgs");
+const imgs = document.querySelectorAll("#imgs div img");
+const fullPage = document.querySelector("#fullpage");
+
+imgs.forEach((img) => {
+  img.addEventListener("click", function () {
+    fullPage.style.backgroundImage = "url(" + img.src + ")";
+    fullPage.style.display = "block";
+  });
+});
